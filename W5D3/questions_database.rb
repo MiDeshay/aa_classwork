c910ba41261e5a825545d64cd4898840e9c705ee
your name here <your_email_hereroelliga@yahoo.com>
2021-08-04 13:41:21 -0700

    questionsdatabse

--- W5D3/questions_database.rb
+++ W5D3/questions_database.rb
@@ -1,11 +1,34 @@
 require "singleton"
 
 class QuestionsDatabase < SQLite3::Database
+    include Singleton 
+    super('questions.db')
+    self.type_translation = true
+    self.results_as_hash = true
+    end
 
 
 end
 
-class Questions
+class Question
+    def self.all
+        data = QuestionsDatabase.instance.execute('SELECT * FROM questions')
+        data.map { |datum| Question.new(datum) }
+    end
+
+    def self.find_by_id(id)
+        QuestionsDatabase.instance.execute(<<-sql, id)
+        
+        sql
+    end
+
+    def initialize(options) 
+        @id = options["id"]
+        @title = options["title"]
+        @body = options["body"]
+        @user_id = options["user_id"]
+    end
+
 
     def find_by_id(id)
 
