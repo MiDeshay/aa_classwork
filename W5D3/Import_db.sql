aa1f54a9c9ea8546941b48fe46e5a1d5683093c5
MiDeshay <86858281+MiDeshay@users.noreply.github.com>
2021-08-04 11:17:19 -0700

    tables done

--- W5D3/Import_db.sql
+++ W5D3/Import_db.sql
@@ -1,3 +1,5 @@
+PRAGMA foreign_keys = ON;
+
 CREATE TABLE users(
     id INTEGER PRIMARY KEY,
     fname TEXT NOT NULL,
@@ -9,9 +11,9 @@ CREATE TABLE questions(
     id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
     body TEXT NOT NULL,
-    associated_author_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
 
-    FOREIGN KEY(associated_author_id) REFERENCES associated_author(id)
+    FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
 CREATE TABLE question_follows(
@@ -30,7 +32,44 @@ CREATE TABLE replies(
     question_id INTEGER NOT NULL,
     parent_id INTEGER,
 
-     FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(user_id) REFERENCES users(id),
     FOREIGN KEY(question_id) REFERENCES questions(id),
     FOREIGN KEY(parent_id) REFERENCES replies(parent_id)
 );
+
+CREATE TABLE question_likes(
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
+    question_like BOOLEAN,
+
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(question_id) REFERENCES questions(id),
+
+)
+
+INSERT INTO 
+    users(fname, lname)
+VALUES
+    ('Michael', 'Deshay')
+    ('Andrew', 'Roellig');
+
+
+INSERT INTO
+    questions(title, body, associated_author_id)
+VALUES
+    ('just wondering', 'what color is the sky?', 1);
+
+
+INSERT INTO
+    question_follows(user_id, question_id)
+VALUES
+    (1, 0);
+
+INSERT INTO
+    replies(body, user_id, question_id, parent_id)
+VALUES
+    ()
+
+
+
