6479562a72aa3c567d21d23890066e51c59b4aea
MiDeshay <86858281+MiDeshay@users.noreply.github.com>
2021-08-03 18:11:41 -0700

    sqlzoo

--- W5D2/skeleton-6/lib/04_select_within_select.rb
+++ W5D2/skeleton-6/lib/04_select_within_select.rb
@@ -35,6 +35,13 @@ end
 def larger_than_russia
   # List each country name where the population is larger than 'Russia'.
   execute(<<-SQL)
+  SELECT name
+  FROM countries
+  WHERE population > (
+    SELECT population
+    FROM countries
+    WHERE name= 'Russia'
+  )
   SQL
 end
 
@@ -42,6 +49,13 @@ def richer_than_england
   # Show the countries in Europe with a per capita GDP greater than
   # 'United Kingdom'.
   execute(<<-SQL)
+  SELECT name
+  FROM countries
+  WHERE gdp/population > (
+    SELECT gdp/population
+    FROM countries
+    WHERE name= 'United Kingdom'
+  ) AND continent= 'Europe'
   SQL
 end
 
@@ -49,6 +63,13 @@ def neighbors_of_certain_b_countries
   # List the name and continent of countries in the continents containing
   # 'Belize', 'Belgium'.
   execute(<<-SQL)
+  SELECT name, continent
+  FROM countries
+  WHERE continent IN (
+    SELECT continent
+    FROM countries
+    WHERE name='Belize' OR name='Belgium'
+  )
   SQL
 end
 
@@ -56,6 +77,17 @@ def population_constraint
   # Which country has a population that is more than Canada but less than
   # Poland? Show the name and the population.
   execute(<<-SQL)
+  SELECT name, population 
+  FROM countries
+  WHERE population > (
+    SELECT population
+    FROM countries
+    WHERE name= 'Canada'
+  ) AND population < (
+    SELECT population
+    FROM countries
+    WHERE name= 'Poland'
+  )
   SQL
 end
 
@@ -65,5 +97,12 @@ def sparse_continents
   # population.
   # Hint: Sometimes rewording the problem can help you see the solution.
   execute(<<-SQL)
+  SELECT name, continent, population
+  FROM countries
+  WHERE continent NOT IN (
+    SELECT continent
+    FROM countries
+    WHERE population >= 25000000
+  )
   SQL
 end
--- W5D2/skeleton-6/lib/05_aggregates.rb
+++ W5D2/skeleton-6/lib/05_aggregates.rb
@@ -22,30 +22,45 @@ end
 def continents
   # List all the continents - just once each.
   execute(<<-SQL)
+  SELECT DISTINCT continent
+  FROM countries
   SQL
 end
 
 def africa_gdp
   # Give the total GDP of Africa.
   execute(<<-SQL)
+  SELECT SUM(gdp)
+  FROM countries
+  WHERE continent = 'Africa'
   SQL
 end
 
 def area_count
   # How many countries have an area of more than 1,000,000?
   execute(<<-SQL)
+  SELECT COUNT(area)
+  FROM countries
+  WHERE area > 1000000
   SQL
 end
 
 def group_population
   # What is the total population of ('France','Germany','Spain')?
   execute(<<-SQL)
+  SELECT SUM(population)
+  FROM countries
+  WHERE name = 'France' OR name = 'Germany' OR name = 'Spain'
   SQL
 end
 
 def country_counts
   # For each continent show the continent and number of countries.
   execute(<<-SQL)
+  SELECT DISTINCT (continent), COUNT(continent)
+
+  FROM countries
+  GROUP BY continent
   SQL
 end
 
@@ -53,6 +68,9 @@ def populous_country_counts
   # For each continent show the continent and number of countries with
   # populations of at least 10 million.
   execute(<<-SQL)
+  SELECT DISTINCT(continent), COUNT(population)
+  FROM countries
+  GROUP BY continent
   SQL
 end
 
