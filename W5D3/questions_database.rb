2c1f4a7be3b40ec1c425f303f6318f1f7b74fc9b
MiDeshay <86858281+MiDeshay@users.noreply.github.com>
2021-08-04 14:13:20 -0700

    added stuff

--- W5D3/questions_database.rb
+++ W5D3/questions_database.rb
@@ -3,9 +3,11 @@ require 'sqlite3'
 
 class QuestionsDatabase < SQLite3::Database
     include Singleton 
-    super('questions.db')
-    self.type_translation = true
-    self.results_as_hash = true
+    def initialize
+        super('questions.db')
+        self.type_translation = true
+        self.results_as_hash = true
+    end
 
 end
 
@@ -13,6 +15,15 @@ class Question
     def self.all
         data = QuestionsDatabase.instance.execute('SELECT * FROM questions')
         data.map { |datum| Question.new(datum) }
+    end
+
+    def self.find_by_author_id(author_id)
+        datum = QuestionsDatabase.instance.execute(<<-sql, author_id)
+                SELECT  *
+                FROM questions
+                WHERE author_id = ?
+            sql
+        Question.new(datum)
     end
 
     def self.find_by_id(id)
@@ -55,4 +66,5 @@ class User
     end
 end
 
-p Question.all
+Question.all
+p User.all
