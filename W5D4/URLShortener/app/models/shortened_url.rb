236e042d6180e9a967545862e66538527054ea2c
David Rodriguez <d.joseph.rodriguez@gmail.com>
2021-08-05 16:45:34 -0700

    Added to ShortenedUrl

--- W5D4/URLShortener/app/models/shortened_url.rb
+++ W5D4/URLShortener/app/models/shortened_url.rb
@@ -4,12 +4,14 @@ class ShortenedUrl < ApplicationRecord
 
     def self.random_code
         random_string = SecureRandom.urlsafe_base64
-        until !ShortenedUrl.exists?(:short_url => random_string)
+        until !ShortenedUrl.exists?(short_url: random_string)
             random_string = SecureRandom.urlsafe_base64
         end
 
         return random_string
     end
+
+    
 
 
 end
