ddf70aefe59b92671d0e370faba3ed7be4a2d548
MiDeshay <86858281+MiDeshay@users.noreply.github.com>
2021-08-05 13:58:13 -0700

    added

--- skeleton-6/app/models/course.rb
+++ skeleton-6/app/models/course.rb
@@ -1,2 +1,6 @@
 class Course < ApplicationRecord
+    has_many :users,
+        primary_key: :id,
+        foreign_key: :student_id,
+        class_name: :User
 end
--- skeleton-6/app/models/enrollment.rb
+++ skeleton-6/app/models/enrollment.rb
@@ -1,2 +1,11 @@
 class Enrollment < ApplicationRecord
+    belongs_to :user,
+        primary_key: :id,
+        foreign_key: :student_id,
+        class_name: :User 
+
+    belongs_to :courses,
+        primary_key: :id,
+        foreign_key: :course_id,
+        class_name: :Course
 end
