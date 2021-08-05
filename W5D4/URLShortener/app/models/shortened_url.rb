14ade8a580ff901af1ed8b454cc970dc6dc1ca93
MiDeshay <86858281+MiDeshay@users.noreply.github.com>
2021-08-05 16:22:00 -0700

    test

--- W5D4/URLShortener/app/models/shortened_url.rb
+++ W5D4/URLShortener/app/models/shortened_url.rb
@@ -4,6 +4,10 @@ class ShortenedUrl < ApplicationRecord
 
     def self.random_code
         random_string = SecureRandom.urlsafe_base64
+        until !ShortenedUrl.exists?(:short_url => random_string)
+            random_string = SecureRandom.urlsafe_base64
+        end
+
         return random_string
     end
 
