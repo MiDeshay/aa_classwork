677c5f375c5d8b7b2eb1d754810cbe9d2d9b7f5b
MiDeshay <86858281+MiDeshay@users.noreply.github.com>
2021-08-13 14:31:37 -0700

    added cats partial

--- W6D5/cats/db/seeds.rb
+++ W6D5/cats/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cat.delete_all
+cat1 = Cat.create(name: 'Mynx', birth_date: '2010/01/12', color: "Red", sex: "F", description: "Likes cuddles")
+cat1 = Cat.create(name: 'Lynn', birth_date: '2012/08/12', color: "Blue", sex: "M", description: "Likes cuddles")
+cat1 = Cat.create(name: 'Princess', birth_date: '2010/05/12', color: "Violet", sex: "F", description: "Likes cuddles")
