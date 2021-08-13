83d925b660fce03e10b41401ad1a78a5d0542781
MiDeshay <86858281+MiDeshay@users.noreply.github.com>
2021-08-13 16:06:40 -0700

    starting edit view

--- W6D5/cats/app/controllers/cats_controller.rb
+++ W6D5/cats/app/controllers/cats_controller.rb
@@ -15,7 +15,7 @@ class CatsController < ApplicationController
         if @cat.save
             redirect_to cat_url(@cat)
         else
-            render json: @cat.errors.full_messages
+            render json: @cat.errors.full_messages, status: 422
         end
     end
 
@@ -23,10 +23,26 @@ class CatsController < ApplicationController
         @cat = Cat.new
         render :new
     end
+    
+    def edit
+        @cat= Cat.find_by(id: params[:id])
+        render :edit
+    end
+
 
     def update
         @cat= Cat.find_by(id: params[:id])
-        render :edit
+        unless @cat
+            render json: {error: 'cat not found'}
+            return
+        end 
+
+        if @cat.update(cat_params)
+            redirect_to cat_url(@cat)
+        else
+            render json:  @cat.errors.full_messages, status: 422
+        end
+
     end
 
     private
