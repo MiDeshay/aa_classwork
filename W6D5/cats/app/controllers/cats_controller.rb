647e762702cb960073e45ced7e8c63a798227ab8
MiDeshay <86858281+MiDeshay@users.noreply.github.com>
2021-08-13 15:14:23 -0700

    starting new view

--- W6D5/cats/app/controllers/cats_controller.rb
+++ W6D5/cats/app/controllers/cats_controller.rb
@@ -10,4 +10,28 @@ class CatsController < ApplicationController
         render :show
     end
 
+    def create
+        @cat = Cat.new(cat_params)
+        if @cat.save
+            redirect_to cat_url(@cat)
+        else
+            render json: @cat.errors.full_messages
+        end
+    end
+
+    def new
+        @cat = Cat.new
+        render :new
+    end
+
+    def update
+        @cat= Cat.find_by(id: params[:id])
+        render :edit
+    end
+
+    private
+
+    def cat_params
+        params.require(:cat).permit(:birth_date, :color, :name, :sex, :description)
+    end
 end
