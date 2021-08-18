a41f9dd2a1fc90840313b00296a6d15ce92395b1
MiDeshay <86858281+MiDeshay@users.noreply.github.com>
2021-08-18 16:20:34 -0700

    starting user controller specs

--- W7D3/GoalApp/app/models/user.rb
+++ W7D3/GoalApp/app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
     validates :username, :password_digest, :email, :session_token, presence: true
     validates :username, :session_token, uniqueness: true
     validates :password, length: {minimum: 6}
+    after_initialize :ensure_session_token
 
     attr_reader :password
 
--- W7D3/GoalApp/spec/controllers/users_controller_spec.rb
+++ W7D3/GoalApp/spec/controllers/users_controller_spec.rb
@@ -10,5 +10,33 @@ RSpec.describe UsersController, type: :controller do
     end
 
 
+    describe 'POST #create' do
+        context 'with valid params' do
+            let(:user_params) do {
+                user: {
+                    username: 'Capy3',
+                    email: "capy3@gmail.com",
+                    password: "password",
+                }
+            }
+         end
 
+         it 'logs in the user' do
+            post :create, params: user_params
+
+            user = User.find_by(username: 'Capy3')
+            expect(session[:session_token]).to eq(user.session_token)
+         end
+
+         it 'redirects to the users show page' do
+            post :create, params: user_params
+
+            user = User.find_by(username: 'Capy3')
+            expect(response).to redirect_to(user_url(user))
+         end
+
+        end
+
+        
+    end
 end
