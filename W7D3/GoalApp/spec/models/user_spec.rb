1d9681e00c32d65671fb7ad52b2cbfa6879fe423
Vern Chao <vernchao91@gmail.com>
2021-08-18 11:51:29 -0700

    starterd GOALAPP

---
+++ W7D3/GoalApp/spec/models/user_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  let(:user) { User.new(
+    username: 'Eli',
+    email: 'eli@capy.com',
+    password: 'password',
+  ) }
+
+  it ""
+end
