265479b3632a8fa6d9f45601ba138d8410ecfa39
Vern Chao <vernchao91@gmail.com>
2021-08-18 14:05:58 -0700

    validation of associations

--- W7D3/GoalApp/app/models/user.rb
+++ W7D3/GoalApp/app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
-    validates :username, presence: true
+    validates :username, :password_digest, :email, :session_token, presence: true
+    validates :username, :session_token, uniqueness: true
+    validates :password, length: {minimum: 6}
+
+    attr_accessor :password
+
+    has_many :goals
+      class_name: :Goal
 end
--- W7D3/GoalApp/spec/models/user_spec.rb
+++ W7D3/GoalApp/spec/models/user_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { User.new(
+subject(:user) { User.new(
     username: 'Eli',
     email: 'eli@capy.com',
-    password: 'password',
+    password: 'password'
+    password_digest: 'g3nk4jgnbd8fgh3k4tn',
+    session_token: '43kfjne98gvbj34gnsaeg'
   ) }
-
+  
   it {should validate_presence_of(:username)}
   it {should validate_presence_of(:password_digest)}
   it {should validate_presence_of(:session_token)}
@@ -16,5 +18,6 @@ RSpec.describe User, type: :model do
   it {should validate_uniqueness_of(:session_token)}
 
   it {should validate_length_of(:password).is_at_least(6)}
+  
 
 end
