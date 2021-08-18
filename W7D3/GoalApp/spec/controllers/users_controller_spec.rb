cc5a5cbf2df0766861c36fcdbe077c087d561e09
Vern Chao <vernchao91@gmail.com>
2021-08-18 15:46:15 -0700

    userscontrollers

--- W7D3/GoalApp/spec/controllers/users_controller_spec.rb
+++ W7D3/GoalApp/spec/controllers/users_controller_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
-    
+    describe 'GET #index' do
+      it 'renders the index template' do
+        get :index
+        expect(response).to render_template(:index)
+      end
+    end
+
+
 
 end
