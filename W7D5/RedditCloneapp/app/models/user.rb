f1ff31d25523f8f6d05fe29ddb193ce148a78d94
MiDeshay <86858281+MiDeshay@users.noreply.github.com>
2021-08-20 09:33:58 -0700

    finished Spire

---
+++ W7D5/RedditCloneapp/app/models/user.rb
@@ -0,0 +1,31 @@
+class User < ApplicationRecord
+    validates :username, :password_digest, :session_token, presence: true
+    validates :username, :session_token, uniqueness: true
+
+    attr_reader :password
+
+    def self.find_by_credentials(username, password)
+        @user = User.find_by(username: username)
+        @user && @user.is_password?(password) ? @user : nil
+    end
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def is_password?(password)
+        password_object = BCrypt::Password.new(self.password_digest)
+        password_object.is_password?(password)
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom.urlsafe_base64
+        self.save!
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom.urlsafe_base64
+    end
+end
