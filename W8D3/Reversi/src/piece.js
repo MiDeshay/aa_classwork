24157159d4b16cf499b936423bdeda6cc6d395d0
MiDeshay <86858281+MiDeshay@users.noreply.github.com>
2021-08-25 11:35:22 -0700

    starting board.js

--- W8D3/Reversi/src/piece.js
+++ W8D3/Reversi/src/piece.js
@@ -2,25 +2,41 @@
  * Initializes the Piece with its color.
  */
 function Piece (color) {
+    this.color = color;
 }
 
 /**
  * Returns the color opposite the current piece.
  */
 Piece.prototype.oppColor = function () {
+    if (this.color === "black"){
+        return "white";
+    }else{
+        return "black";
+    }
 };
 
 /**
  * Changes the piece's color to the opposite color.
  */
 Piece.prototype.flip = function () {
+    if(this.color === "black"){
+        this.color = "white";
+    }else{
+        this.color = "black";
+    }
 };
 
 /**
- * Returns a string representation of the string
+ * Returns a letter representation of the string
  * based on its color.
  */
 Piece.prototype.toString = function () {
+    if (this.color === "black"){
+        return "B";
+    }else{
+        return "W";
+    }
 };
 
 // DON'T TOUCH THIS CODE
