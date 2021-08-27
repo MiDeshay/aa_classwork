4870316648dda6c66e41d71fefae98aeb9dd13c9
MiDeshay <86858281+MiDeshay@users.noreply.github.com>
2021-08-26 17:22:21 -0700

    last switch

--- W8D4/absurdBubbleSort.js
+++ W8D4/absurdBubbleSort.js
@@ -15,27 +15,36 @@ function askIfGreaterThan(el1, el2, callback){
     })
 }
 
-// askIfGreaterThan(3, 5, variable  => {
-//     console.log(variable);
-//     reader.close();
-// })
 
 
 function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop){
     if (i === arr.length - 1){
-            outerBubbleSortLoop(madeAnySwaps);
+        outerBubbleSortLoop(madeAnySwaps);
+    } else {
+        askIfGreaterThan(arr[i], arr[i + 1], function (isGreaterThan){
+        if (isGreaterThan){
+            madeAnySwaps = true;
+            const temporary = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = temporary;
+        }
+    })
+        innerBubbleSortLoop(arr, i + 1, madeAnySwaps, outerBubbleSortLoop );
     }
 
-        askIfGreaterThan(arr[i], arr[i + 1], function (isGreaterThan){
-            if (isGreaterThan){
-                const temporary = arr[i];
-                arr[i] = arr[i + 1];
-                arr[i + 1] = temporary;
-            }
+  
 
-        }
-        )
+
 }
+// function absurdBubbleSort(arr, sortCompletionCallback){
+//     function outerBubbleSortLoop(madeAnySwaps){
+        
+//     }
+// }
+let arr = [2,5,6,7,9,4,3,7,1]
 
+innerBubbleSortLoop(arr, 0, false, function (){
+    console.log("In outer bubble sort");
+    reader.close();
+});
 
-fjaslfdjsldfjsdl
