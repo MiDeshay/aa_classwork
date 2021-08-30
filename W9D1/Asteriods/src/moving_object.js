18400096dbe5885fee54c27c15fe0e80a7f4e9dc
MiDeshay <86858281+MiDeshay@users.noreply.github.com>
2021-08-30 11:37:32 -0700

    draw

--- W9D1/Asteriods/src/moving_object.js
+++ W9D1/Asteriods/src/moving_object.js
@@ -5,4 +5,20 @@ function MovingObject(positon, velocity, radius, color) {
     this.color = color;
 }
 
+
+MovingObject.prototype.draw = function (ctx){
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+
+    ctx.arc(
+        this.positon[0],
+        this.positon[1],
+        this.radius,
+        0,
+        2 * Math.PI,
+        false
+    );
+    ctx.fill();
+}
+
 module.exports = MovingObject;
