81b85a71788e6d33dcc50fc4226ec83c86e989db
IanDMcGrath <ian92415@gmail.com>
2021-09-13 11:14:57 -0700

    hello world

---
+++ W11D1/Minesweeper/components/game.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Game extends React.Component{
+  
+  render () {
+    
+    return (
+    <>"hello world"</>
+    );
+  }
+}
+
+export default Game;
---
+++ W11D1/Minesweeper/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = { // es5 style
+	context: __dirname,
+	entry: './entry.jsx',
+	output: {			// where webpack should output hte bundled files
+		path: path.resolve(__dirname),
+		// use path.resolve() to resolve our paths into an absolute path.
+		// __dirname tells us the absolute path of teh directory containing the currently executing file
+		filename: 'bundle.js'
+	},
+	devtool: 'source-map', // a source map makes it possible for you to see the line numbers of your original source code when errors are raised. ~~~
+	resolve: {
+		extensions: ['.js', '.jsx', '*']
+	},
+  module: { // allows you to specify several loaders within our webpack configuration
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: { // 'query:' or 'options:' depending on webpack version read the errrors
+            presets: ['@babel/env', '@babel/react']
+          }
+        }
+      }
+    ]
+  }
+}
