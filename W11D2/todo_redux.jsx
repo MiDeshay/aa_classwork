33c993ddca2fb62b87d15362e87c923304559900
tkla <kennyla95@gmail.com>
2021-09-14 14:17:32 -0700

    Added reducers and store

--- W11D2/todo_redux.jsx
+++ W11D2/todo_redux.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDom from 'react-dom';
+import configureStore from './frontend/store/store'
 
 document.addEventListener("DOMContentLoaded", ()=>{
+    const store = configureStore();
+    window.store = store; 
     ReactDom.render(<h1>Todos App</h1>, document.getElementById('root'))
 })
