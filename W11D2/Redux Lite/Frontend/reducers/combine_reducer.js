56e4d19441fe2187da6b1f9b4f20e9ff583ac579
MiDeshay <86858281+MiDeshay@users.noreply.github.com>
2021-09-14 17:17:25 -0700

    redux combine reducer

--- W11D2/Redux Lite/Frontend/reducers/combine_reducer.js
+++ W11D2/Redux Lite/Frontend/reducers/combine_reducer.js
@@ -4,6 +4,10 @@ const combineReducer = (reducers = {
         users: userReducer,
         roles: roleReducer 
     }) =>{
-    
+    const root = (prevState, action) => {
+        prevState.keys.forEach(ele => {
+            
+        }); 
+    }
     return
 }
