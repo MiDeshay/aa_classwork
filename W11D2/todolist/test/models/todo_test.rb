fdbcfb4b131626e42e21a2c68e24aa497eadb3d9
tkla <kennyla95@gmail.com>
2021-09-16 14:58:34 -0700

    Annotated models

--- W11D2/todolist/test/models/todo_test.rb
+++ W11D2/todolist/test/models/todo_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: todos
+#
+#  id         :bigint           not null, primary key
+#  body       :string           not null
+#  done       :boolean          not null
+#  title      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require 'test_helper'
 
 class TodoTest < ActiveSupport::TestCase
