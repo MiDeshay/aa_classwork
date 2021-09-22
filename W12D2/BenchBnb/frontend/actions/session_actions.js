d48cd387d7b0dabfa6b0a954184e79ed7a71bb37
JoeJoegb922 <josephlam922@gmail.com>
2021-09-21 17:54:21 -0700

    finished thunk actions

--- W12D2/BenchBnb/frontend/actions/session_actions.js
+++ W12D2/BenchBnb/frontend/actions/session_actions.js
@@ -5,16 +5,16 @@ export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
 export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
 
 
- const receiveCurrentUser = (user) => ({
+const receiveCurrentUser = (user) => ({
     type: RECEIVE_CURRENT_USER, 
     user 
 })
 
- const logoutCurrentUser = () => ({
+const logoutCurrentUser = () => ({
     type: LOGOUT_CURRENT_USER
 })
 
- const receiveErrors = (errors) => ({
+export const receiveErrors = (errors) => ({
     type: RECEIVE_ERRORS,
     errors: errors
 })
@@ -22,4 +22,13 @@ export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
 export const loginUser = (user) => dispatch => {
     login(user).then((response) => dispatch(receiveCurrentUser(response)))
 }
+
+export const logoutUser = () => dispatch => {
+    logout().then(() => dispatch(logoutCurrentUser()))
+}
+
+export const signUpUser = (user) => dispatch => {
+    signUp(user).then((res) => dispatch(receiveCurrentUser(res)));
+}
+
 
