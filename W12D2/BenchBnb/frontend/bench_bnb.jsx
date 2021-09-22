196a491c0414c556e1b521af3bf042dc952c4780
JoeJoegb922 <josephlam922@gmail.com>
2021-09-21 17:11:43 -0700

    starting front end auth

--- W12D2/BenchBnb/frontend/bench_bnb.jsx
+++ W12D2/BenchBnb/frontend/bench_bnb.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { login, logout, signUp } from "./util/session_api_util";
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
+  window.signUp = signUp;
+  window.login = login;
+  window.logout = logout;
+  
   ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
 });
